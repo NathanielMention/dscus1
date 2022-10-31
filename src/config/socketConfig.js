a46a6195c5cd0d288c842a1085a3f64c77ef7117
Nathaniel Mention <nathanielmention@gmail.com>
2022-10-30 23:23:48 -0400

    .

--- src/config/socketConfig.js
+++ src/config/socketConfig.js
@@ -1,7 +1,4 @@
 import openSocket from "socket.io-client";
-let socket = openSocket("http://localhost:5001");
-if (process.env.NODE_ENV === "production") {
-  socket = openSocket("https://dscus.onrender.com:5001");
-}
+let socket = openSocket("https://dscus.onrender.com:5001");
 
 export default socket;
